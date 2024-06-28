cf580294e3cb814ccef3118f28df9453b11ecfad
Isolynx (a.k.a GetMbr) <isolynx.getmbr@gmail.com>
2024-06-28 17:07:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // Visual Studio 2022 Version 17 (17.6.33829.357)
 //: H:\V1\func.cpp 
 
-/// NOTE: If you notice that there are 2 function in finding the file, it was used with other types of strings,
+/// NOTE: If you notice that there are 2 functions in finding the file, it was used with other types of strings,
 /// like the wide string (ends with W) which made up of wide characters, wide characters supports unicode 
 /// code page or format, while multi byte (ends with A) supports only ANSI code page or format which doesn't
 /// support unicode.
