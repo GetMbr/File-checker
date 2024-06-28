83d175fba2fa94df47ae522d281f671d41a91b11
Isolynx (a.k.a GetMbr) <isolynx.getmbr@gmail.com>
2024-06-28 11:18:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,50 @@
 // Visual Studio 2022 Version 17 (17.6.33829.357)
-// Used for: H:\V1\func.cpp 
+//: H:\V1\func.cpp 
 
-#define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN // exclude useless headers
 #include <windows.h>
 #include <Shellapi.h>
 #include <winternl.h>
 
-namespace V1 {
-	BOOL matchesW(LPCWSTR string1, LPCWSTR string2) {
+namespace V1 
+{
+	BOOL matchesW(LPCWSTR string1, LPCWSTR string2) 
+        {
 		INT szStr1, szStr2, szStringCount = 0;
-		szStr1 = lstrlenW(string1),	szStr2 = lstrlenW(string2);
-		if (szStr1 > szStr2 && szStr1 < szStr2) return FALSE;
-		for (int i = 0; i < szStr2; i++) {
+		szStr1 = lstrlenW(string1),
+		szStr2 = lstrlenW(string2);
+		
+		if (szStr1 > szStr2 && szStr1 < szStr2)
+			return FALSE;
+		
+		for (int i = 0; i < szStr2; i++)
+		{
 			if (string1[i] == string2[i]) szStringCount++;
 		}
-		if (szStringCount < szStr2) return FALSE;
+		
+		if (szStringCount < szStr2) 
+			return FALSE;
+		
 		return TRUE;
 	}
 
-	BOOL matchesA(LPCSTR string1, LPCSTR string2) {
+	BOOL matchesA(LPCSTR string1, LPCSTR string2) 
+        {
 		INT szStr1, szStr2, szStringCount = 0;
-		szStr1 = lstrlenA(string1), szStr2 = lstrlenA(string2);
-		if (szStr1 > szStr2 && szStr1 < szStr2) return FALSE;
-		for (int i = 0; i < szStr2; i++) {
+		szStr1 = lstrlenA(string1), 
+		szStr2 = lstrlenA(string2);
+		
+		if (szStr1 > szStr2 && szStr1 < szStr2)
+			return FALSE;
+		
+		for (int i = 0; i < szStr2; i++)
+		{
 			if (string1[i] == string2[i]) szStringCount++;
 		}
-		if (szStringCount < szStr2) return FALSE;
+		
+		if (szStringCount < szStr2) 
+			return FALSE;
+		
 		return TRUE;
 	}
 
@@ -34,6 +53,7 @@ namespace V1 {
 		WIN32_FIND_DATAW np; BOOL success = FALSE;
 		LPWSTR sd = (LPWSTR)LocalAlloc(LMEM_ZEROINIT, MAX_PATH + 1), 
 			ssfile = (LPWSTR)LocalAlloc(LMEM_ZEROINIT, 512);
+		
 		lstrcpyW(sd, lPath);
 		if (sd[(lstrlenW(sd) - 1)] != L'\\')
 		{
@@ -42,11 +62,13 @@ namespace V1 {
 			}
 		}
 		lstrcatW(sd, L"*.*");
+		
 		HANDLE hFile = FindFirstFileW(sd, &np);
 		if (hFile == INVALID_HANDLE_VALUE)
 			goto cleanup;
 
-		while (FindNextFileW(hFile, &np)) {
+		while (FindNextFileW(hFile, &np)) 
+		{
 			ssfile = (LPWSTR)LocalAlloc(LMEM_ZEROINIT, 512);
 			lstrcpyW(ssfile, np.cFileName);
 			if (!lstrcmpW(np.cFileName, L".") || !lstrcmpW(np.cFileName, L"..") || np.dwFileAttributes == FILE_ATTRIBUTE_REPARSE_POINT) continue;
@@ -73,6 +95,7 @@ namespace V1 {
 		WIN32_FIND_DATAA np; BOOL success = FALSE;
 		LPSTR sd = (LPSTR)LocalAlloc(LMEM_ZEROINIT, MAX_PATH + 1),
 		      ssfile = (LPSTR)LocalAlloc(LMEM_ZEROINIT, 512);
+		
 		lstrcpyA(sd, lPath);
 		if (sd[(lstrlenA(sd) - 1)] != '\\')
 		{
@@ -81,11 +104,13 @@ namespace V1 {
 			}
 		}
 		lstrcatA(sd, "*.*");
+		
 		HANDLE hFile = FindFirstFileA(sd, &np);
 		if (hFile == INVALID_HANDLE_VALUE)
 			goto cleanup;
 
-		while (FindNextFileA(hFile, &np)) {
+		while (FindNextFileA(hFile, &np)) 
+		{
 			ssfile = (LPSTR)LocalAlloc(LMEM_ZEROINIT, 512);
 			lstrcpyA(ssfile, np.cFileName);
 			if (!lstrcmpA(np.cFileName, ".") || !lstrcmpA(np.cFileName, "..") || np.dwFileAttributes == FILE_ATTRIBUTE_REPARSE_POINT) continue;
@@ -108,55 +133,12 @@ namespace V1 {
 	}
 }
 
-int main() {
+int main() 
+{
+    // just use std::cout if its a console application you were using this.
     if (V1::CheckIfFileExistsA("C:\\", "test.txt"))
         MessageBoxA(0, "found!", "test.txt", MB_OK | MB_ICONINFORMATION);
     else 
         MessageBoxA(0, "not found...", "test.txt", MB_OK | MB_ICONERROR);
     return 0;
 }
-
-/*
-███╗░░░███╗░█████╗░███╗░░██╗██╗░░██╗██╗███╗░░██╗██████╗░
-████╗░████║██╔══██╗████╗░██║██║░██╔╝██║████╗░██║██╔══██╗
-██╔████╔██║███████║██╔██╗██║█████═╝░██║██╔██╗██║██║░░██║
-██║╚██╔╝██║██╔══██║██║╚████║██╔═██╗░██║██║╚████║██║░░██║
-██║░╚═╝░██║██║░░██║██║░╚███║██║░╚██╗██║██║░╚███║██████╔╝
-╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚═════╝░
-
-██╗░██████╗    ██████╗░███████╗░█████╗░██████╗░
-██║██╔════╝    ██╔══██╗██╔════╝██╔══██╗██╔══██╗
-██║╚█████╗░    ██║░░██║█████╗░░███████║██║░░██║
-██║░╚═══██╗    ██║░░██║██╔══╝░░██╔══██║██║░░██║
-██║██████╔╝    ██████╔╝███████╗██║░░██║██████╔╝
-╚═╝╚═════╝░    ╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░
-
-██████╗░██╗░░░░░░█████╗░██████╗░██████╗░      ██╗░██████╗
-██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗      ██║██╔════╝
-██████╦╝██║░░░░░██║░░██║██║░░██║██║░░██║      ██║╚█████╗░
-██╔══██╗██║░░░░░██║░░██║██║░░██║██║░░██║      ██║░╚═══██╗
-██████╦╝███████╗╚█████╔╝██████╔╝██████╔╝      ██║██████╔╝
-╚═════╝░╚══════╝░╚════╝░╚═════╝░╚═════╝░      ╚═╝╚═════╝░
-
-███████╗██╗░░░██╗███████╗██╗░░░░░
-██╔════╝██║░░░██║██╔════╝██║░░░░░
-█████╗░░██║░░░██║█████╗░░██║░░░░░
-██╔══╝░░██║░░░██║██╔══╝░░██║░░░░░
-██║░░░░░╚██████╔╝███████╗███████╗
-╚═╝░░░░░░╚═════╝░╚══════╝╚══════╝
-
-██╗░░██╗███████╗██╗░░░░░██╗░░░░░░  ██╗░██████╗
-██║░░██║██╔════╝██║░░░░░██║░░░░░░  ██║██╔════╝
-███████║█████╗░░██║░░░░░██║░░░░░░  ██║╚█████╗░
-██╔══██║██╔══╝░░██║░░░░░██║░░░░░░  ██║░╚═══██╗
-██║░░██║███████╗███████╗███████╗░  ██║██████╔╝
-╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝░  ╚═╝╚═════╝░
-
-███████╗  ██╗░░░██╗  ██╗░░░░░  ██╗░░░░░
-██╔════╝  ██║░░░██║  ██║░░░░░  ██║░░░░░
-█████╗░░  ██║░░░██║  ██║░░░░░  ██║░░░░░
-██╔══╝░░  ██║░░░██║  ██║░░░░░  ██║░░░░░
-██║░░░░░  ╚██████╔╝  ███████╗  ███████╗
-╚═╝░░░░░  ░╚═════╝░  ╚══════╝  ╚══════╝
-*/
-	
