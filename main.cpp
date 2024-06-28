ff4c4c62815cb643ba36c5c460fd3d96cdcd2bf1
Isolynx (a.k.a GetMbr) <isolynx.getmbr@gmail.com>
2024-06-28 17:06:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 // Visual Studio 2022 Version 17 (17.6.33829.357)
 //: H:\V1\func.cpp 
 
-/// NOTE: If you notice that there are 2 function in finding the file, it was used on other types of strings,
-/// like the wide string (ends with W) which made up of wide characters, wide chsracters supports unicode 
-/// code page or format, while multi byte (ends with A) supports only ANSI code page or format.
+/// NOTE: If you notice that there are 2 function in finding the file, it was used with other types of strings,
+/// like the wide string (ends with W) which made up of wide characters, wide characters supports unicode 
+/// code page or format, while multi byte (ends with A) supports only ANSI code page or format which doesn't
+/// support unicode.
 
 #define WIN32_LEAN_AND_MEAN // exclude useless headers
 #include <windows.h>
